01b5731ae5815f145ad1dd751bd06703b89d29e9
Phan Tran Thai Son <1512853@hcmut.edu.vn>
2018-05-05 20:50:20 +0700

    add UI, class Level

--- Bloxorz.py
+++ Bloxorz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys
+import sys, pygame
 
 ########################################################################################################################
 # Map description:
@@ -30,6 +30,13 @@ import sys
 
 import time
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
 LEVEL1_ARRAY = [
     [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
@@ -40,8 +47,8 @@ LEVEL1_ARRAY = [
 ]
 LEVEL2_ARRAY = [
     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
-    [1, 1, 1, 1, 0, 0, 1, 1, 3, 1, 0, 0, 1, 4, 1],
-    [1, 1, -3, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 1, 0, 0, 1, 1, 2, 1, 0, 0, 1, 4, 1],
+    [1, 1, -2, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
     [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
     [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
     [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0]
@@ -91,28 +98,27 @@ LEVEL6_ARRAY = [
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0]
 ]
 LEVEL7_ARRAY = [
-    [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0],
-	[0,0,0,0,0,0,0,0,1,1,1,1,0,0,0],
-	[1,1,1,0,0,0,0,0,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,0,0,0,1,4,1],
-	[1,1,1,0,0,0,0,1,1,3,0,0,1,1,1],
-	[1,1,1,0,0,0,0,1,1,1,0,0,1,1,1],
-	[0,1,1,0,0,0,0,1,0,0,0,0,0,0,0],
-	[0,0,1,1,1,1,1,1,0,0,0,0,0,0,0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 4, 1],
+    [1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
 ]
-LEVEL25_ARRAY= [
-    [0,0,1,1,0,0,0,0,0,0,0,0,0,0],
-	[0,0,1,1,1,0,0,0,0,0,0,0,0,0],
-	[0,0,1,1,10,0,0,0,0,0,1,1,1,0],
-	[0,0,0,1,1,1,1,0,0,0,1,4,1,0],
-	[0,0,0,0,0,0,1,1,0,0,1,1,1,0],
-	[0,1,1,0,0,0,1,1,0,0,0,0,0,0],
-	[1,1,20,1,1,1,1,1,0,0,0,0,0,0],
-	[1,1,1,0,0,0,1,1,0,0,0,1,1,1],
-	[1,1,1,0,0,0,1,1,10,1,1,1,1,1],
-	[0,0,0,0,0,0,0,0,0,0,0,1,1,1]
+LEVEL25_ARRAY = [
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, -2, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 4, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
 ]
-
 
 
 ########################################################################################################################
@@ -156,6 +162,7 @@ class State:
         self.states = [start]
         self.xo_objects = xo_objects
         self.visited = [start]
+        self.start = start
 
     # self.all_moves = self.next_position()
 
@@ -165,28 +172,16 @@ class State:
     def next_position(self, prev_node):
         rv = []
         if self.is_stand():
-            # rv.append(Node((self.x0, self.y0 + 1, self.x1, self.y1 + 2), prev_node, "down", prev_node.map))
-            # rv.append(Node((self.x0, self.y0 - 1, self.x0, self.y0 - 2), prev_node, "up", prev_node.map))
-            # rv.append(Node((self.x0 + 1, self.y0, self.x0 + 2, self.y0), prev_node, "right", prev_node.map))
-            # rv.append(Node((self.x0 - 1, self.y0, self.x0 - 2, self.y1), prev_node, "left", prev_node.map))
             self.add_move(rv, (self.x0, self.y0 + 1, self.x1, self.y1 + 2), prev_node, "down")
             self.add_move(rv, (self.x0, self.y0 - 1, self.x0, self.y0 - 2), prev_node, "up")
             self.add_move(rv, (self.x0 + 1, self.y0, self.x0 + 2, self.y0), prev_node, "right")
             self.add_move(rv, (self.x0 - 1, self.y0, self.x0 - 2, self.y1), prev_node, "left")
         elif self.x0 == self.x1:
-            # rv.append(Node((self.x0 + 1, self.y0, self.x1 + 1, self.y1), prev_node, "right", prev_node.map))
-            # rv.append(Node((self.x0 - 1, self.y0, self.x1 - 1, self.y1), prev_node, "left", prev_node.map))
-            # rv.append(Node((self.x0, self.y0 - 1, self.x1, self.y1 - 2), prev_node, "up", prev_node.map))
-            # rv.append(Node((self.x0, self.y0 + 2, self.x1, self.y1 + 1), prev_node, "down", prev_node.map))
             self.add_move(rv, (self.x0 + 1, self.y0, self.x1 + 1, self.y1), prev_node, "right")
             self.add_move(rv, (self.x0 - 1, self.y0, self.x1 - 1, self.y1), prev_node, "left")
             self.add_move(rv, (self.x0, self.y0 - 1, self.x1, self.y1 - 2), prev_node, "up")
             self.add_move(rv, (self.x0, self.y0 + 2, self.x1, self.y1 + 1), prev_node, "down")
         elif self.y0 == self.y1:
-            # rv.append(Node((self.x0, self.y0 + 1, self.x1, self.y1 + 1), prev_node, "down", prev_node.map))
-            # rv.append(Node((self.x0, self.y0 - 1, self.x1, self.y1 - 1), prev_node, "up", prev_node.map))
-            # rv.append(Node((self.x0 - 1, self.y0, self.x1 - 2, self.y1), prev_node, "left", prev_node.map))
-            # rv.append(Node((self.x0 + 2, self.y0, self.x1 + 1, self.y1), prev_node, "right", prev_node.map))
             self.add_move(rv, (self.x0, self.y0 + 1, self.x1, self.y1 + 1), prev_node, "down")
             self.add_move(rv, (self.x0, self.y0 - 1, self.x1, self.y1 - 1), prev_node, "up")
             self.add_move(rv, (self.x0 - 1, self.y0, self.x1 - 2, self.y1), prev_node, "left")
@@ -198,10 +193,11 @@ class State:
     def add_move(self, rv, data, prev_node, direction):
         for xo_object in self.xo_objects:
             if (data[0] == xo_object.position[0] and data[1] == xo_object.position[1]) or (
-                    data[2] == xo_object.position[0] and data[3] == xo_object.position[1]):
+                            data[2] == xo_object.position[0] and data[3] == xo_object.position[1]):
                 for m in xo_object.managed_position:
                     if (xo_object.type == XOObject.TYPE_O) or (
-                            xo_object.type == XOObject.TYPE_X and data[0] == data[2] and data[1] == data[3]):
+                                        xo_object.type == XOObject.TYPE_X and data[0] == data[2] and data[1] == data[
+                                3]):
                         if m.type == ManagedPosition.BOTH:
                             xo_objects_states = dict(prev_node.xo_objects_states)
                             xo_objects_states[(m.x, m.y)] = not xo_objects_states[(m.x, m.y)]
@@ -223,15 +219,6 @@ class State:
     # Function to check if the object repeated previous move (which could lead to infinite loop)
     # Created: SonPhan 23/04/2018
     ####################################################################################################################
-    # def notContain(self, node):
-    #     while prev:
-    #         if node.data[0] == prev.data[0] and node.data[1] == prev.data[1] and node.data[2] == prev.data[2] \
-    #                 and node.data[3] == prev.data[3]:
-    #             if node.xo_objects_states == prev.xo_objects_states:
-    #                 end = time.time()
-    #                 return False
-    #         prev = prev.prev_node
-    #     return True
     def notContain(self, node):
         for n in self.visited:
             if n.data[0] == node.data[0] and n.data[1] == node.data[1] and n.data[2] == node.data[2] \
@@ -286,7 +273,7 @@ class State:
         self.x0, self.y0, self.x1, self.y1 = node.data
         for xo_object in self.xo_objects:
             if (self.x0 == xo_object.position[0] and self.y0 == xo_object.position[1]) or (
-                    self.x1 == xo_object.position[0] and self.y1 == xo_object.position[1]):
+                            self.x1 == xo_object.position[0] and self.y1 == xo_object.position[1]):
                 for m in xo_object.managed_position:
                     if xo_object.type == XOObject.TYPE_O or (xo_object.type == XOObject.TYPE_X and self.is_stand()):
                         if m.type == ManagedPosition.BOTH:
@@ -326,10 +313,8 @@ def bfs(state):
     current_state = Node
     # BFS operation
     while len(state.states) != 0:
-        # print(len(state.states))
+
         current_state = state.states.pop(0)
-        # if current_state.data[0] == 1 and current_state.data[1] == 4 and current_state.data[2] == 1 and current_state.data[3] == 5:
-        #     print(1)
         state.set_player_position(current_state)
         if state.check_goal():
             break
@@ -338,60 +323,151 @@ def bfs(state):
     path = []
     # Backtracking all the previous moves to reach this goal state
     while pointer:
-        path.insert(0, pointer.action)
+        path.insert(0, pointer)
         pointer = pointer.prev_node
     # And print them out
-    for action in path:
-        print(action)
-    # end = time.time()
-    # print(end - start)
+    for p in path:
+        print(p.action)
+    return path
+
+
+def draw_map(screen, node):
+    # height = len(map)
+    # width = len(map[0])
+    # for row in map:
+    #     for col in row:
+    #         if col == 1:
+    #             pygame.draw.rect(screen, WHITE, [30 + 75 * row.index(col), 30 + 75 * map.index(row), 75, 75], 0)
+    #             pygame.draw.rect(screen, BLACK, [30 + 75 * row.index(col), 30 + 75 * map.index(row), 75, 75], 1)
+    map = node.map
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if (i == node.data[1] and j == node.data[0]) or (i == node.data[3] and j == node.data[2]):
+                pygame.draw.rect(screen, BLUE, [30 + 75 * j, 30 + 75 * i, 75, 75], 0)
+                pygame.draw.rect(screen, BLACK, [30 + 75 * j, 30 + 75 * i, 75, 75], 1)
+            elif map[i][j] == 1:
+                pygame.draw.rect(screen, WHITE, [30 + 75 * j, 30 + 75 * i, 75, 75], 0)
+                pygame.draw.rect(screen, BLACK, [30 + 75 * j, 30 + 75 * i, 75, 75], 1)
+            elif map[i][j] == -2:
+                pygame.draw.rect(screen, WHITE, [30 + 75 * j, 30 + 75 * i, 75, 75], 0)
+                pygame.draw.rect(screen, BLACK, [30 + 75 * j, 30 + 75 * i, 75, 75], 1)
+                pygame.draw.circle(screen, RED, [30 + 75 * j + 37, 30 + 75 * i + 37], 30)
+            elif map[i][j] == 2:
+                pygame.draw.rect(screen, WHITE, [30 + 75 * j, 30 + 75 * i, 75, 75], 0)
+                pygame.draw.rect(screen, BLACK, [30 + 75 * j, 30 + 75 * i, 75, 75], 1)
+                pygame.draw.aaline(screen, RED, [30 + 75 * j, 30 + 75 * i], [30 + 75 * j + 75, 30 + 75 * i + 75], 1)
+                pygame.draw.aaline(screen, RED, [30 + 75 * j, 30 + 75 * i + 75], [30 + 75 * j + 75, 30 + 75 * i], 1)
+            elif map[i][j] == 5:
+                pygame.draw.rect(screen, YELLOW, [30 + 75 * j, 30 + 75 * i, 75, 75], 0)
+                pygame.draw.rect(screen, BLACK, [30 + 75 * j, 30 + 75 * i, 75, 75], 1)
+
+
+class Level:
+    def __init__(self, level_array, state, xo_objects=[]):
+        self.level_array = level_array
+        self.state = state
+        self.xo_objects = xo_objects
+
+
+def init_levels():
+    levels_array = []
+    for i in range(33):
+        levels_array.append(None)
+    # LEVEL1 SOLVER:
+    state1 = State(Node((1, 1, 1, 1), None, "", LEVEL1_ARRAY), LEVEL1_ARRAY)
+    levels_array[0] = Level(LEVEL1_ARRAY, state1)
+
+    # LEVEL2 SOLVER:
+    xo_objects2 = [
+        XOObject(XOObject.TYPE_O, (2, 2),
+                 [ManagedPosition(4, 4, ManagedPosition.BOTH), ManagedPosition(5, 4, ManagedPosition.BOTH)]),
+        XOObject(XOObject.TYPE_X, (8, 1),
+                 [ManagedPosition(10, 4, ManagedPosition.BOTH), ManagedPosition(11, 4, ManagedPosition.BOTH)])]
+    state2 = State(
+        Node((1, 4, 1, 4), None, "", LEVEL2_ARRAY, {(4, 4): False, (5, 4): False, (10, 4): False, (11, 4): False}),
+        LEVEL2_ARRAY, xo_objects2)
+    levels_array[1] = Level(LEVEL2_ARRAY, state2, xo_objects2)
+
+    # LEVEL3 SOLVER:
+    state3 = State(Node((1, 3, 1, 3), None, "", LEVEL3_ARRAY), LEVEL3_ARRAY)
+    levels_array[2] = Level(LEVEL3_ARRAY, state3)
+
+    # LEVEL4 SOLVER:
+    state4 = State(Node((1, 5, 1, 5), None, "", LEVEL4_ARRAY), LEVEL4_ARRAY)
+    levels_array[3] = Level(LEVEL4_ARRAY, state4)
+
+    # Level 5 Solver :
+    xo_objects5 = [XOObject(XOObject.TYPE_O, (8, 1),
+                            [ManagedPosition(5, 1, ManagedPosition.BOTH), ManagedPosition(6, 1, ManagedPosition.BOTH)]),
+                   XOObject(XOObject.TYPE_O, (3, 3), [ManagedPosition(5, 8, ManagedPosition.ONLY_ENABLE),
+                                                      ManagedPosition(6, 8, ManagedPosition.ONLY_ENABLE)]),
+                   XOObject(XOObject.TYPE_O, (6, 5), [ManagedPosition(5, 8, ManagedPosition.ONLY_DISABLE),
+                                                      ManagedPosition(6, 8, ManagedPosition.ONLY_DISABLE)]),
+                   XOObject(XOObject.TYPE_O, (14, 6), [
+                       ManagedPosition(5, 8, ManagedPosition.BOTH), ManagedPosition(6, 8, ManagedPosition.BOTH)])]
+    state5 = State(
+        Node((13, 1, 13, 1), None, "", LEVEL5_ARRAY, {(5, 1): True, (6, 1): True, (5, 8): True, (6, 8): True}),
+        LEVEL5_ARRAY, xo_objects5)
+    levels_array[4] = Level(LEVEL5_ARRAY, state5, xo_objects5)
+
+    # LEVEL6 SOLVER:
+    state6 = State(Node((0, 3, 0, 3), None, "", LEVEL6_ARRAY), LEVEL6_ARRAY)
+    levels_array[5] = Level(LEVEL6_ARRAY, state6)
+
+    # LEVEL7 SOLVER
+    xo_objects7 = [XOObject(XOObject.TYPE_X, (9, 4), [ManagedPosition(3, 6, ManagedPosition.BOTH)])]
+    state7 = State(Node((1, 3, 1, 3), None, "", LEVEL7_ARRAY, {(3, 6): False}), LEVEL7_ARRAY, xo_objects7)
+    levels_array[6] = Level(LEVEL7_ARRAY, state7, xo_objects7)
+
+    # LEVEL25 SOLVER
+    xo_objects25 = [XOObject(XOObject.TYPE_O, (4, 2),
+                             [ManagedPosition(8, 4, ManagedPosition.BOTH), ManagedPosition(9, 4, ManagedPosition.BOTH),
+                              ManagedPosition(13, 2, ManagedPosition.BOTH),
+                              ManagedPosition(13, 3, ManagedPosition.BOTH)]), XOObject(XOObject.TYPE_X, (2, 6), [
+        ManagedPosition(8, 4, ManagedPosition.ONLY_ENABLE), ManagedPosition(9, 4, ManagedPosition.ONLY_ENABLE)]),
+                    XOObject(XOObject.TYPE_O, (8, 8), [ManagedPosition(4, 6, ManagedPosition.ONLY_DISABLE),
+                                                       ManagedPosition(5, 6, ManagedPosition.ONLY_DISABLE),
+                                                       ManagedPosition(7, 3, ManagedPosition.ONLY_ENABLE)])]
+    state25 = State(Node((1, 7, 1, 7), None, "", LEVEL25_ARRAY,
+                         {(4, 6): True, (5, 6): True, (7, 3): False, (8, 4): False, (9, 4): False, (13, 2): False,
+                          (13, 3): False}),
+                    LEVEL25_ARRAY, xo_objects25)
+    levels_array[24] = Level(LEVEL25_ARRAY, state25,xo_objects25)
+
+    return levels_array
 
 
 def main():
-    # LEVEL1 SOLVER:
-    # state = State(Node((1, 1, 1, 1), None, "", LEVEL1_ARRAY), LEVEL1_ARRAY)
+    levels_array = init_levels()
+    level_choice = int(input("Nhap level: "))
+    if levels_array[level_choice-1] is None:
+        print("Chua lam level nay :v")
+        return
 
-    # LEVEL2 SOLVER:
-    # xo_objects = [
-    #     XOObject(XOObject.TYPE_O, (2, 2),
-    #              [ManagedPosition(4, 4, ManagedPosition.BOTH), ManagedPosition(5, 4, ManagedPosition.BOTH)]),
-    #     XOObject(XOObject.TYPE_X, (8, 1),
-    #              [ManagedPosition(10, 4, ManagedPosition.BOTH), ManagedPosition(11, 4, ManagedPosition.BOTH)])]
-    # state = State(
-    #     Node((1, 4, 1, 4), None, "", LEVEL2_ARRAY, {(4, 4): False, (5, 4): False, (10, 4): False, (11, 4): False}),
-    #     LEVEL2_ARRAY, xo_objects)
+    done = False
+    path = bfs(levels_array[level_choice - 1].state)
+    pygame.init()
+    pygame.display.set_caption("Bloxorz")
+    height = len(levels_array[level_choice - 1].level_array)
+    width = len(levels_array[level_choice - 1].level_array[0])
+    screen = pygame.display.set_mode((width * 100, height * 100))
+    i = 0
+    while not done:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    if i < len(path) - 1:
+                        i += 1
+                elif event.key == pygame.K_LEFT:
+                    if i > 0:
+                        i -= 1
+        screen.fill(GREEN)
 
-    # LEVEL3 SOLVER:
-    # state = State(Node((1, 3, 1, 3), None, "", LEVEL3_ARRAY), LEVEL3_ARRAY)
-
-    # LEVEL4 SOLVER:
-    # state = State(Node((1, 5, 1, 5), None, "", LEVEL4_ARRAY), LEVEL4_ARRAY)
-
-    # Level 5 Solver :
-    # xo_objects = [XOObject(XOObject.TYPE_O, (8, 1),
-    #                        [ManagedPosition(5, 1, ManagedPosition.BOTH), ManagedPosition(6, 1, ManagedPosition.BOTH)]),
-    #               XOObject(XOObject.TYPE_O, (3, 3), [ManagedPosition(5, 8, ManagedPosition.ONLY_ENABLE),
-    #                                                  ManagedPosition(6, 8, ManagedPosition.ONLY_ENABLE)]),
-    #               XOObject(XOObject.TYPE_O, (6, 5), [ManagedPosition(5, 8, ManagedPosition.ONLY_DISABLE),
-    #                                                  ManagedPosition(6, 8, ManagedPosition.ONLY_DISABLE)]),
-    #               XOObject(XOObject.TYPE_O, (14, 6), [
-    #                   ManagedPosition(5, 8, ManagedPosition.BOTH), ManagedPosition(6, 8, ManagedPosition.BOTH)])]
-    # state = State(
-    #     Node((13, 1, 13, 1), None, "", LEVEL5_ARRAY, {(5, 1): True, (6, 1): True, (5, 8): True, (6, 8): True}),
-    #     LEVEL5_ARRAY, xo_objects)
-
-    # LEVEL6 SOLVER:
-    # state = State(Node((0, 3, 0, 3), None, "", LEVEL6_ARRAY), LEVEL6_ARRAY)
-
-    xo_objects = [XOObject(XOObject.TYPE_X, (9, 4),[ManagedPosition(3, 6, ManagedPosition.BOTH)])]
-    state = State(Node((1, 3, 1, 3), None, "", LEVEL7_ARRAY, {(3, 6): False}), LEVEL7_ARRAY, xo_objects)
-
-    #LV25
-    # xo_objects = [XOObject(XOObject.TYPE_O, (4, 2),[ManagedPosition(8, 4, ManagedPosition. BOTH),ManagedPosition(9,4,ManagedPosition.BOTH),ManagedPosition(13,2,ManagedPosition.BOTH),ManagedPosition(13,3,ManagedPosition.BOTH)]),XOObject(XOObject.TYPE_X,(2,6),[ManagedPosition(8, 4,ManagedPosition.ONLY_ENABLE),ManagedPosition(9,4,ManagedPosition.ONLY_ENABLE)]),XOObject(XOObject.TYPE_O,(8,8),[ManagedPosition(4,6,ManagedPosition.ONLY_DISABLE),ManagedPosition(5,6,ManagedPosition.ONLY_DISABLE),ManagedPosition(7,3,ManagedPosition.ONLY_ENABLE)])]
-    # state = State(Node((1,7, 1, 7), None, "", LEVEL25_ARRAY, {(4, 6): True,(5,6):True,(7,3):False,(8,4):False,(9,4):False,(13,2):False,(13,3):False}),
-    #               LEVEL25_ARRAY, xo_objects)
-
-    bfs(state)
+        draw_map(screen, path[i])
+        pygame.display.flip()
 
 
 main()
+
